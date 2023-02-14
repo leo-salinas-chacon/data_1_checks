21327f9b775f7966c4e371603a096cdcf61f56cb
Leonardo D Salinas Chacon <leonardo.salinaschacon@gmail.com>
2023-02-13 22:49:47 -0500

    Printing to my hearts desire

---
+++ test_1.py
@@ -0,0 +1,11 @@
+print("Hello World")
+
+A_list = ["program",42,400.78]
+print(A_list[2])
+
+dict_1 = {'FF7' : 1997,
+'Crash Bandicoot': 1996 }
+print(dict_1['Crash Bandicoot'])
+
+tuple_1 = ['Metroid Prime Remaster', 'Legend of Zelda: Tears of the Kingdom','Octopath Traveler 2','Advance Wars: ELectric Boogaloo']
+print(tuple_1[3])
